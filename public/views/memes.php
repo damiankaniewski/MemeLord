d54089565729c09a7e20fcbe3876df3944e1acaf
unknown <daba9@op.pl>
2023-02-03 22:48:45 +0100

    Reformating code

---
+++ public/views/memes.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/memes.css">
+
+    <script src="https://kit.fontawesome.com/32e257cbac.js" crossorigin="anonymous"></script>
+    <title>MEMES</title>
+</head>
+
+<body>
+<div class="base-container">
+    <nav>
+        <img src="../img/logo.svg">
+        <ul>
+            <li>
+                <i class="fa-solid fa-images"></i>
+                <a href="#" class="button">Memes</a>
+            </li>
+            <li>
+                <i class="fa-solid fa-people-group"></i>
+                <a href="#" class="button">People</a>
+            </li>
+            <li>
+                <i class="fa-regular fa-comments"></i>
+                <a href="#" class="button">Messages</a>
+            </li>
+            <li>
+                <i class="fa-solid fa-comment"></i>
+                <a href="#" class="button">Notifications</a>
+            </li>
+        </ul>
+    </nav>
+    <main>
+        <header>
+            <div class="search-bar">
+                <form>
+                    <input placeholder="Search memes">
+                </form>
+            </div>
+            <div class="add-meme">
+                <i class="fas fa-plus"></i>
+                Add new meme
+            </div>
+        </header>
+        <section class="memes">
+            <div id="meme-1">
+                <img src="../img/meme1.png">
+                <div>
+                    <h2>nice meme bro</h2>
+                    <p>#meme #bro #funny</p>
+                    <div class="social-section">
+                        <i class="fa-solid fa-thumbs-up"> 44</i>
+                        <i class="fa-solid fa-thumbs-down"> 2</i>
+                    </div>
+                </div>
+            </div>
+            <div>Have you seen it?</div>
+            <div>TiTlE oF MeMe</div>
+            <div>Whooooooa</div>
+        </section>
+    </main>
+</div>
+</body>
